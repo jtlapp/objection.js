e6028305d848b9eb6707df5e7e6822d82122b6a7
Joe Lapp <arachnojoe@gmail.com>
2018-01-06 13:27:52 -0600

    starting koa-inversify from express-ts

---
+++ examples/koa-inversify/src/app.ts
@@ -0,0 +1,43 @@
+import registerApi from './api';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import * as promiseRouter from 'express-promise-router';
+import * as Knex from 'knex';
+import * as morgan from 'morgan';
+import {Model} from 'objection';
+
+const knexConfig = require('../knexfile');
+// Initialize knex.
+export const knex = Knex(knexConfig.development);
+
+// Create or migrate:
+knex.migrate.latest();
+
+// Bind all Models to a knex instance. If you only have one database in
+// your server this is all you have to do. For multi database systems, see
+// the Model.bindKnex method.
+Model.knex(knex);
+
+const router = promiseRouter();
+const app = express()
+  .use(bodyParser.json())
+  .use(morgan('dev'))
+  .use(router)
+  .set('json spaces', 2);
+
+// Register our REST API.
+registerApi(router);
+
+// Error handling. The `ValidationError` instances thrown by objection.js have a `statusCode`
+// property that is sent as the status code of the response.
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (err) {
+    res.status(err.statusCode || err.status || 500).send(err.data || err.message || {});
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(8641, function() {
+  console.log('Example app listening at port %s', server.address().port);
+});
---
+++ examples/koa-inversify/src/models/Person.ts
@@ -0,0 +1,145 @@
+import {Model, RelationMappings} from 'objection';
+import {join} from 'path';
+
+export interface Address {
+  street: string;
+  city: string;
+  zipCode: string;
+}
+
+export default class Person extends Model {
+  readonly id: number;
+  parent: Person;
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+  createdAt: Date;
+  updatedAt: Date;
+
+  // Table name is the only required property.
+  static tableName = 'Person';
+
+  // Optional JSON schema. This is not the database schema! Nothing is generated
+  // based on this. This is only used for validation. Whenever a model instance
+  // is created it is checked against this schema. http://json-schema.org/.
+  static jsonSchema = {
+    type: 'object',
+    required: ['firstName', 'lastName'],
+
+    properties: {
+      id: {type: 'integer'},
+      parentId: {type: ['integer', 'null']},
+      firstName: {type: 'string', minLength: 1, maxLength: 255},
+      lastName: {type: 'string', minLength: 1, maxLength: 255},
+      age: {type: 'number'},
+
+      address: {
+        type: 'object',
+        properties: {
+          street: {type: 'string'},
+          city: {type: 'string'},
+          zipCode: {type: 'string'}
+        }
+      }
+    }
+  };
+
+  // Where to look for models classes.
+  static modelPaths = [__dirname];
+
+  // This object defines the relations to other models. The modelClass strings
+  // will be joined to `modelPaths` to find the class definition, to avoid
+  // require loops. The other solution to avoid require loops is to make
+  // relationMappings a thunk. See Movie.ts for an example.
+  static relationMappings: RelationMappings = {
+    pets: {
+      relation: Model.HasManyRelation,
+      // This model defines the `modelPaths` property. Therefore we can simply use
+      // the model module names in `modelClass`.
+      modelClass: 'Animal',
+      join: {
+        from: 'Person.id',
+        to: 'Animal.ownerId'
+      }
+    },
+
+    movies: {
+      relation: Model.ManyToManyRelation,
+      modelClass: 'Movie',
+      join: {
+        from: 'Person.id',
+        // ManyToMany relation needs the `through` object to describe the join table.
+        through: {
+          from: 'Person_Movie.personId',
+          to: 'Person_Movie.movieId'
+        },
+        to: 'Movie.id'
+      }
+    },
+
+    children: {
+      relation: Model.HasManyRelation,
+      modelClass: Person,
+      join: {
+        from: 'Person.id',
+        to: 'Person.parentId'
+      }
+    },
+
+    parent: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: Person,
+      join: {
+        from: 'Person.parentId',
+        to: 'Person.id'
+      }
+    }
+  };
+
+  examplePersonMethod(arg: string): number {
+    return 1;
+  }
+
+  //
+  // Example of numeric timestamps. Presumably this would be in a base
+  // class or a mixin, and not just one of your leaf models.
+  //
+
+  $beforeInsert() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date();
+  }
+
+  $parseDatabaseJson(json: object) {
+    json = super.$parseDatabaseJson(json);
+    toDate(json, 'createdAt');
+    toDate(json, 'updatedAt');
+    return json;
+  }
+
+  $formatDatabaseJson(json: object) {
+    json = super.$formatDatabaseJson(json);
+    toTime(json, 'createdAt');
+    toTime(json, 'updatedAt');
+    return json;
+  }
+}
+
+function toDate(obj: any, fieldName: string): any {
+  if (obj != null && typeof obj[fieldName] === 'number') {
+    obj[fieldName] = new Date(obj[fieldName]);
+  }
+  return obj;
+}
+
+function toTime(obj: any, fieldName: string): any {
+  if (obj != null && obj[fieldName] != null && obj[fieldName].getTime) {
+    obj[fieldName] = obj[fieldName].getTime();
+  }
+  return obj;
+}
