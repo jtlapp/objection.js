1ea2ab10279b137f138594a9ae598798f8831825
Joe Lapp <arachnojoe@gmail.com>
2018-01-14 16:08:37 -0600

    removed old koa-inversify dir

--- examples/koa-inversify/src/deployments/Deployment.ts
+++
@@ -1,5 +0,0 @@
-import * as Koa from 'koa';
-
-export interface Deployment {
-    config(app: Koa): void;
-}
--- examples/koa-inversify/src/repos/MovieStarRepo.ts
+++
@@ -1,9 +0,0 @@
-import {PersonBroker} from '../brokers/PersonBroker';
-import {AnimalBroker} from '../brokers/AnimalBroker';
-import {MovieBroker} from '../brokers/MovieBroker';
-
-export interface MovieStarRepo {
-    personBroker: PersonBroker;
-    animalBroker: AnimalBroker;
-    movieBroker: MovieBroker;
-}
